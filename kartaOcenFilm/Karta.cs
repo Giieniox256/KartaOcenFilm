2fe6a6b70de672d11942f81d5e5b722d306789c0
petro_fpv <kowalik.piotr256@gmail.com>
2020-05-11 22:52:13 +0200

    Podawanie ocen

--- kartaOcenFilm/Karta.cs
+++ kartaOcenFilm/Karta.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace kartaOcenFilm
 {
     public class Karta
     {
-        public Karta()
-        {
-            lock;
+        List<float> oceny = new List<float>();
 
+
+
+        /// <summary>
+        /// dodaje ocene
+        /// </summary>
+        /// <param name="ocena"> nowa ocena</param>
+        public void DodajOcene(float ocena)
+        {
+            oceny.Add(ocena);
         }
+        /// <summary>
+        /// oblicza srednią ocen
+        /// </summary>
+        /// <returns>zwraca średnią</returns>
+        public float ObliczSrednia()
+        {
+            float suma = 0;
+            float srednia = 0;
+
+            foreach (var ocena in oceny)
+            {
+                suma += ocena;
+            }
+            srednia = suma / oceny.Count();
+
+            return srednia;
+        }
+        /// <summary>
+        /// zwraca najwyższą ocene
+        /// </summary>
+        public float MaxOcena()
+        {
+            return oceny.Max();
+        }
+        /// <summary>
+        /// zwraca najniższą ocenę
+        /// </summary>
+        /// <returns></returns>
+        public float MinOcena()
+        {
+            return oceny.Min();
+        }
+    
+           
+        
     }
 }
--- kartaOcenFilm/Program.cs
+++ kartaOcenFilm/Program.cs
@@ -6,7 +6,50 @@ namespace kartaOcenFilm
     {
         public static void Main(string[] args)
         {
-            
+            Karta karta = new Karta();
+            Karta kartaUzytkownika = new Karta();
+
+            karta.DodajOcene(5);
+            karta.DodajOcene(3);
+            karta.DodajOcene(8);
+            karta.DodajOcene(3.5f);
+            karta.DodajOcene(6.5f);
+
+            float srednia = karta.ObliczSrednia();
+            float maxVal = karta.MaxOcena();
+            float minVal = karta.MinOcena();
+
+            for (; ; )
+            {
+                float ocena;
+                Console.WriteLine("Podaj ocene z zakresu 1 - 10");
+                bool wynik = float.TryParse(Console.ReadLine(), out ocena);
+
+                if (wynik)
+                {
+                    if (ocena == 11)
+                    {
+                        break;
+                    }
+
+                    if (ocena>0 && ocena <=10)
+                    {
+                        kartaUzytkownika.DodajOcene(ocena);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Liczba spoza zakresu!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("To nie jest liczba");
+                }
+            }
+            Console.WriteLine("Srednia liczba to: " + kartaUzytkownika.ObliczSrednia());
+            Console.WriteLine("Nawiększa ocena to: " + kartaUzytkownika.MaxOcena());
+            Console.WriteLine("Najmniejsza ocena to: " + kartaUzytkownika.MinOcena());
+
 
         }
     }
